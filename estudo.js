b0f2417c73e15ddee2b26cf459f8978fdb434d6a
Leninj007 <108944939+Leninj007@users.noreply.github.com>
2022-10-07 12:48:53 -0300

    Aula 07-10

--- estudo.js
+++ estudo.js
@@ -1,1 +1,13 @@
-
+function total(){
+    let c = document.getElementById("capital").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let m = 0;
+    for(let i = 1; i <= t; i++){
+         m = c * (1 + (j/100));
+         document.write("valor no mês " + i + " = " + m + "<br>");
+         c = m;
+    }
+    
+    document.write("Montante: " + m);
+}
